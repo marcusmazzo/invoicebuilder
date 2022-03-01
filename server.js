4b9ac742d0bbee7968003315b8cc91a95dcfe3c0
Marcus Laprano <marcus.mazzo@gmail.com>
2022-03-01 15:47:45 +0000

    correction heroku 2

--- server.js
+++ server.js
@@ -3,5 +3,4 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/invoicebuilder'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/bolsa-front/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/invoicebuilder/index.html'));});
