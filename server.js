dffc52fb2348ac6842a38fbf0fb8caeb85aafd51
Marcus Laprano <marcus.mazzo@gmail.com>
2022-03-01 16:14:30 +0000

    correction heroku 5

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/invoicebuilder'));
+app.use(express.static(__dirname + '/dist/invoice-win'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/invoicebuilder/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/invoice-win/index.html'));});
+app.listen(process.env.PORT || 8080); 
+console.log("server running")
